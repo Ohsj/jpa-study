faef0335c8640818eaee4011cbf8e75c1104a856
ohsh <zasqw456@naver.com>
2021-01-04 18:28:22 +0900

    jpa-study project init

---
+++ src/main/kotlin/com/osj4532/jpastudy/JpaStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.osj4532.jpastudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<JpaStudyApplication>(*args)
+}
