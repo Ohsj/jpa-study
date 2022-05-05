4965cb29475a2accc573e4e3740c4c2dcbd0f28c
ohsj <zasqw456@naver.com>
2022-05-05 19:48:48 +0900

    Feat: add memberService api and test

---
+++ src/main/java/me/study/jpa/v1/service/MemberService.java
@@ -0,0 +1,48 @@
+package me.study.jpa.v1.service;
+
+import lombok.RequiredArgsConstructor;
+import me.study.jpa.v1.entity.Member;
+import me.study.jpa.v1.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    // 빈 주입 시 필드 주입 대신 생성자 주입 방식 권장
+    // 변경 불가능한 객체 생성 가능
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원 가입
+     */
+    @Transactional // 변경
+    public Long join(Member member) {
+        validateDuplicateMember(member);    // 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
