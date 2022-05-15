410e22d49c05e90e8e6a286b2f4b3c5f2edb8b96
ohsj <zasqw456@naver.com>
2022-05-15 16:43:36 +0900

    Feat: add member join form and controller

---
+++ src/main/java/me/study/jpa/v1/controller/MemberController.java
@@ -0,0 +1,45 @@
+package me.study.jpa.v1.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.study.jpa.v1.entity.Address;
+import me.study.jpa.v1.entity.Member;
+import me.study.jpa.v1.model.MemberForm;
+import me.study.jpa.v1.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+}
