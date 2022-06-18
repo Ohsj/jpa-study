e7ba8cd42b2d94dda1cba9e8c0e942d85ee723f4
ohsj <zasqw456@naver.com>
2022-06-18 20:57:08 +0900

    Feat: Named 쿼리 작성하기

--- src/main/java/me/study/jpa/v2/entity/Member.java
+++ src/main/java/me/study/jpa/v2/entity/Member.java
@@ -11,6 +11,11 @@ import javax.persistence.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 // @ToString은 가급적 내부 필드만(연관관계 없는 필드만
 @ToString(of = {"id", "username", "age"})
+
+@NamedQuery(
+        name = "Member.findByUsername",
+        query="select m from Member m where m.username = :username"
+)
 public class Member {
 
     @Id @GeneratedValue
--- src/main/java/me/study/jpa/v2/repository/MemberRepository.java
+++ src/main/java/me/study/jpa/v2/repository/MemberRepository.java
@@ -2,10 +2,16 @@ package me.study.jpa.v2.repository;
 
 import me.study.jpa.v2.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    // @Query를 생략하고 메서드 이름만으로 Named 쿼리 호출 가능
+//    @Query(name = "Member.findByUsername")
+    List<Member> findByUsername(@Param("username") String username);
 }
