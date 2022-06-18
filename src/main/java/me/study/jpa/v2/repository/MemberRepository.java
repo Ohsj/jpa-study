5660995e12b3aba601d2aa2657a87968b40ed6c5
ohsj <zasqw456@naver.com>
2022-06-18 20:52:04 +0900

    Feat: 메소드 이름으로 쿼리 생성 비교

--- src/main/java/me/study/jpa/v2/repository/MemberRepository.java
+++ src/main/java/me/study/jpa/v2/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package me.study.jpa.v2.repository;
 import me.study.jpa.v2.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
