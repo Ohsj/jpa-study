77d277911d2bff049224ea6c2ea5049710579d22
ohsj <zasqw456@naver.com>
2022-06-26 19:14:03 +0900

    Feat: 파라미터 바인딩, 컬렉션 바인딩

--- src/main/java/me/study/jpa/v2/repository/MemberRepository.java
+++ src/main/java/me/study/jpa/v2/repository/MemberRepository.java
@@ -8,6 +8,20 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
+/**
+ * 조회 결과가 많거나 없으면?
+ * 컬렉션
+ * 결과 없음: 빈 컬렉션 반환
+ * 단건 조회
+ * 결과 없음: null 반환
+ * 결과가 2건 이상: javax.persistence.NonUniqueResultException 예외 발생
+ *
+ * 단건으로 지정한 메서드를 호출하면 스프링 데이터 JPA는 내부에서 JPQL의
+ * Query.getSingleResult() 메서드를 호출한다. 이 메서드를 호출했을 때 조회 결과가 없으면
+ * javax.persistence.NoResultException 예외가 발생하는데 개발자 입장에서 다루기가 상당히
+ * 불편하다. 스프링 데이터 JPA는 단건을 조회할 때 이 예외가 발생하면 예외를 무시하고 대신에 null 을
+ * 반환한다.
+ */
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
@@ -24,4 +38,13 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     // DTO로 직접 조회
     @Query("select new me.study.jpa.v2.model.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
     List<MemberDto> findMemberDto();
+
+    // 파라미터 바인딩
+    // 코드 가독성 및 유지 보수에 파라미터 바인딩이 좋다.
+    @Query("select m from Member m where m.username = :name")
+    Member findMembers(@Param("name") String username);
+
+    // 컬렉션 파라미터 바인딩
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") List<String> names);
 }
